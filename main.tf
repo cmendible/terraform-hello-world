74615591370a5b5d064b4a7d142a4999a6cb99f3
cmendible <cmendible@gmail.com>
2020-06-11 15:38:56 +0200

    Adding a resource group and backend so module deploys something and generates state

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "everiscoe"
+
+    workspaces {
+      name = "test-workspace-github"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tfe-test-generated"
+  location = "West Europe"
+}
+
 output "hello_world" {
   value = "Hello, World! ${var.your_name} kudos tou you!!!"
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "your_name" {
   type        = string
   description = "Your name"
 }
+
+variable "tfe_host" {
+  description = "tfe host"
+}
+
+variable "tfe_org_name" {
+  description = "tfe organization name"
+}
+
+variable "tfe_workspace_name" {
+  description = "tfe workspace"
+}
